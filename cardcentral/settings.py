2fff7421680ecb04b17f41ab939c5a6eb780ae55
marklaban36 <marklaban32@gmail.com>
2025-10-26 15:33:25 +0000

    Set allowed hosts

--- cardcentral/settings.py
+++ cardcentral/settings.py
@@ -30,9 +30,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '.herokuapp.com'
-]
+ALLOWED_HOSTS = ['.herokuapp.com',
+                 '127.0.0.1',]
+
 
 
 # Application definition
