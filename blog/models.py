dfcef41ab1324dd54accfaee6dea7144ebcc1e34
marklaban36 <marklaban32@gmail.com>
2025-10-28 18:04:47 +0000

    Add summernote to admin create models and base html

--- blog/models.py
+++ blog/models.py
@@ -1,37 +1,50 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-STATUS = ((0, "Draft"), (1, "Published"))
-
-# Create your models here.
+DRAFT = 0
+PUBLISHED = 1
+STATUS = (
+    (DRAFT, "Draft"),
+    (PUBLISHED, "Published"),
+)
 
 
 class Post(models.Model):
+    # Basic identity
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
+
+    # Relations
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+
+    # Content
     content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
+
+    # Timestamps and status
+    created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=DRAFT)
 
     class Meta:
         ordering = ["-created_on"]
 
     def __str__(self):
-        return f"{self.title} | written by {self.author}"
+        return "{} | written by {}".format(self.title, self.author)
+
+    @property
+    def is_published(self):
+        return self.status == PUBLISHED
+
+    def get_excerpt(self, max_len=200):
+        if self.excerpt:
+            return self.excerpt
+        return (self.content[: max_len - 3] + "...") if len(self.content) > max_len else self.content
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments"
-    )
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter"
-    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
@@ -40,4 +53,8 @@ class Comment(models.Model):
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.body} by {self.author}"
+        return "Comment {!r} by {}".format(self.body, self.author)
+
+    def approve(self):
+        self.approved = True
+        self.save()
