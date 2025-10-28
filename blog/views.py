78b7fe2211d6d3eb8704f25bb50b23a38f0ac2fb
marklaban36 <marklaban32@gmail.com>
2025-10-28 08:41:07 +0000

    Fixed issues with page loading

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
+
 from django.shortcuts import render
 from django.views import generic
 from .models import Post
 
-
 # Create your views here.
 
 
-class Publicpost(generic.ListView):
-    model = Post
-    template_name = 'blog/public_post.html'
+class PostList(generic.ListView):
+    queryset = Post.objects.all()
+    template_name = "public_post.html"
