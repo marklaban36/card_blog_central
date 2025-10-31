2950f7ca9e527f16d03309d6b10e87f649046a9a
marklaban36 <marklaban32@gmail.com>
2025-10-31 10:41:14 +0000

    Add temp styles

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-
 from django.shortcuts import render
 from django.views import generic
+from django.views.generic import DetailView
 from .models import Post
 
 # Create your views here.
@@ -9,3 +9,4 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/public_post.html"
+    paginate_by = 20
