2f8e77ca806f72be5348b5c6eced5fc59185def8
marklaban36 <marklaban32@gmail.com>
2025-11-02 17:40:40 +0000

    fixing more bugs

--- blog/admin.py
+++ blog/admin.py
@@ -11,7 +11,6 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status', 'created_on',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
-    prepopulated_fields = {'slug': ('title',)}
 
 
 # Register your models here.
