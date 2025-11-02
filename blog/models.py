a5e89ab0f00912e23e8cb2470c369df1ada7079f
marklaban36 <marklaban32@gmail.com>
2025-11-02 15:19:07 +0000

    fixed issue for deployment were getting error 500

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 DRAFT = 0
 PUBLISHED = 1
@@ -12,7 +13,7 @@ STATUS = (
 class Post(models.Model):
     # Basic identity
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
 
     # Relations
     author = models.ForeignKey(
@@ -47,8 +48,24 @@ class Post(models.Model):
             return self.content[: max_len - 3] + "..."
         return self.content
 
+    def save(self, *args, **kwargs):
+        # Auto-generate or update slug from title
+        base_slug = slugify(self.title)
+        slug = base_slug
+        counter = 1
+
+        # Check if slug is changing or missing
+        if not self.slug or slugify(self.title) != self.slug:
+            while Post.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = slug
+
+        super().save(*args, **kwargs)
+
 
 class Comment(models.Model):
+
     post = models.ForeignKey(
         Post,
         on_delete=models.CASCADE,
