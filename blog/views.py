a442f80c01eca234a8b4cc0f96f12f7e59812abd
marklaban36 <marklaban32@gmail.com>
2025-11-03 14:47:23 +0000

    Fix issue with page load

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ from .forms import CommentForm
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
-    template_name = "blog/index.html"
+    template_name = "blog/public_post.html"
     paginate_by = 6
 
 
